93ece93f437253f12eeaa2b525de38274650a3d2
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 14:23:12 +0530

    Add files via upload

---
+++ Exercise1.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Enter the maximum possible marks: ");
+        Scanner sc = new Scanner(System.in);
+        int max = sc.nextInt();
+        System.out.println("Enter subject1 marks: ");
+        float sub1 = sc.nextFloat();
+        System.out.println("Enter subject2 marks: ");
+        float sub2 = sc.nextFloat();
+        System.out.println("Enter subject3 marks: ");
+        float sub3 = sc.nextFloat();
+        System.out.println("Enter subject4 marks: ");
+        float sub4 = sc.nextFloat();
+        System.out.println("Enter subject5 marks: ");
+        float sub5 = sc.nextFloat();
+        float total = sub1+sub2+sub3+sub4+sub5;
+        float per = (total/max)*100;
+        System.out.println("Your percentage is: "+per);
+    }
+}
