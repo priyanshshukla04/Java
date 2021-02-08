687a75ecdc77140345a20dfb162b3ad5ce478bc5
Priyansh Shukla <60940500+priyanshshukla04@users.noreply.github.com>
2021-02-08 14:20:13 +0530

    Add files via upload

---
+++ FirstCode.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class FirstCode {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("The sum of these is");
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int sum = num1+num2+num3;
+        System.out.println(sum);
+
+    }
+}
