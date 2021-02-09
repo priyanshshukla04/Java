530857ef0dd350410952d60d871600dadea5dede
Priyansh Shukla <priyanshshukla4799@gmail.com>
2021-02-09 20:30:19 +0530

    Add files via upload

---
+++ Relational_Logical.java
@@ -0,0 +1,29 @@
+public class Relational_Logical {
+    public static void main(String[] args) {
+        System.out.println("For Logical AND...");
+        boolean a = true;
+        boolean b = false;
+//        if (a && b){
+//            System.out.println("Y");
+//        }
+//        else{
+//            System.out.println("N");
+//        }
+
+        System.out.println("For Logical OR...");
+
+//        if (a || b){
+//            System.out.println("Y");
+//        }
+//        else{
+//            System.out.println("N");
+//        }
+
+        System.out.println("For Logical NOT");
+        System.out.print("Not(a) is ");
+        System.out.println(!a);
+        System.out.print("Not(b) is ");
+        System.out.println(!b);
+            }
+        }
+
