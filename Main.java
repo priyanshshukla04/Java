c5aa94c1fe1756971abca994ea4c34186a658c30
Priyansh Shukla <priyanshshukla4799@gmail.com>
2021-04-24 18:02:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,29 @@
+package com.company;
+class Phone{
+    public void time(){
+        System.out.println("The time is 5:48 pm");
+    }
+    public void Turn_on(){
+        System.out.println("Turning on Phone...");
+    }
+}
+
+class SmartPhone extends Phone{
+    public void music(){
+        System.out.println("Playing music...");
+    }
+    public void Turn_on(){
+        System.out.println("Turning on Smart phone...");
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Phone obj = new SmartPhone();
+        obj.time();
+        obj.Turn_on();
+    //  obj.music();    //not allowed
+    }
+}
